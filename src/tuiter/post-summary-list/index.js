811509937c0aabd71e708fc50ac2c3b88173b046
Raymond <raymond.eah@gmail.com>
2022-11-21 01:25:53 -0500

    fixes explore

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -3,11 +3,12 @@ import PostSummaryItem from "./post-summary-item";
 import {useSelector} from "react-redux";
 
 const PostSummaryList = () => {
-    const postsArray = useSelector(state => state.tuits)
+    const {tuits} = useSelector(state => state.tuitsData);
+    
     return(
         <ul className="list-group">
             {
-                postsArray.map(post =>
+                tuits.map(post =>
                     <PostSummaryItem
                     key={post._id} post={post}/> )
             }
