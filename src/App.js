8f4dc44c0c47db8c3bc0521e1aafd43bd3298879
Raymond <raymond.eah@gmail.com>
2022-11-21 02:09:01 -0500

    add redirects file, change index

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
       <BrowserRouter>
           <div className="container">
               <Routes>
-                  <Route path="/*" index
+                  <Route path="/*"
                         element={<Labs/>}/>
                   <Route path="/hello"
                         element={<HelloWorld/>}/>
-                  <Route path="/tuiter/*"
+                  <Route path="/tuiter/*" index
                         element={<Tuiter/>}/>
               </Routes>
           </div>
