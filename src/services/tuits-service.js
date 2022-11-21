1b9f0c503855e150fcea478bf9b266023df72608
Raymond <raymond.eah@gmail.com>
2022-11-21 01:47:54 -0500

    hello???

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 // const API_BASE = 'http://localhost:4000/api'
-const TUITS_API = `${API_BASE}`;
+const TUITS_API = `${API_BASE}/tuits`;
 console.log(TUITS_API);
 
 export const createTuit = async (tuit) => {
