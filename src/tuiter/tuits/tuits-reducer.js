678cbcef73a33e64e7726ae971da8865723c237a
Raymond <raymond.eah@gmail.com>
2022-11-21 01:32:22 -0500

    removes unused vars

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -5,23 +5,6 @@ const initialState = {
     tuits: [],
     loading: false
 }
- 
-const currentUser = {
-    "userName": "NASA",
-    "handle": "@nasa",
-    "image": "nasa.png",
-};
-
-const templateTuit = {
-    ...currentUser,
-    "topic": "Space",
-    "time": "2h",
-    "liked": false,
-    "replies": 0,
-    "retuits": 0,
-    "likes": 0,
-};
-   
 
 const tuitsSlice = createSlice({
     name: 'tuits',
