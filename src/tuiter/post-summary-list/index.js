66417c26a7c3d5ea5d3b5791161ef165c853fc50
Raymond <raymond.eah@gmail.com>
2022-11-21 02:15:03 -0500

    buggy explore

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PostSummaryItem from "./post-summary-item";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import { findTuitsThunk } from "../../services/tuits-thunks";
 
 const PostSummaryList = () => {
     const {tuits} = useSelector(state => state.tuitsData);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findTuitsThunk());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     
     return(
         <ul className="list-group">
